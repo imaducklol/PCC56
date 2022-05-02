a6ffbb8117016a3e297252d923791bff1f8b7e67
imaducklol <orionhess@gmail.com>
2022-05-02 13:59:39 -0700

    W4P2 completed and turned in

--- W4P2/W4P2.cpp
+++ W4P2/W4P2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <algorithm>
 using namespace std;
 
 int main () {
+    // Setup
     mt19937 generator(time(NULL));
     uniform_int_distribution<int> ztoh(0, 100);
     int count;
@@ -21,5 +23,30 @@ int main () {
 
     cout << "{";
     for (int i = 0; i < count; i++) cout << numbersToSort[i] << ",";
+    cout << "}\n";
+
+    // Sorting
+    int index;
+    int hold;
+    for (int i = 0; i < count; i++) {
+        index = i; 
+        while (index > 0 && numbersToSort[index] < numbersToSort[index - 1]) {
+            hold = numbersToSort[index];
+            numbersToSort[index] = numbersToSort[index - 1];
+            numbersToSort[index - 1] = hold;
+            index--;
+            
+        }
+    }
+
+    // Checking and output
+    cout << "mine {";
+    for (int i = 0; i < count; i++) cout << numbersToSort[i] << ",";
+    cout << "}\n";
+
+    sort(copyForChecking, (copyForChecking + count));
+
+    cout << "copy {";
+    for (int i = 0; i < count; i++) cout << copyForChecking[i] << ",";
     cout << "}\n";
 }
